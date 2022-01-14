227beec31c71ca9fc1b7c5fbdeb7a5d6e7954625
andrew <andrew@DESKTOP-1QPBKNV>
2022-01-14 20:46:10 +0200

    Add project files.

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class TreeNode
+    {
+        public int Data { get; set; }
+        public TreeNode LeftNode;
+        public TreeNode RightNode;
+        public void Insert(int value)
+        {
+            if(value >= Data)
+            {
+                if(RightNode == null)
+                {
+                    RightNode = new TreeNode { Data = value };
+                }
+                else
+                {
+                    RightNode.Insert(value);
+                }
+            }
+            else
+            {
+                if(LeftNode == null)
+                {
+                    LeftNode = new TreeNode { Data = value };   
+                }
+                else
+                {
+                    LeftNode.Insert(value);
+                }
+            }
+        }
+        public void CLR(TreeNode node, List<int> result)
+        {
+            if(node != null)
+            {
+                result.Add(node.Data);
+                CLR(node.LeftNode, result);
+                CLR(node.RightNode, result);
+            }
+        }
+        public void LCR(TreeNode node, List<int> result)
+        {
+            if(node != null)
+            {
+                LCR(node.LeftNode, result);
+                result.Add(node.Data);
+                LCR(node.RightNode, result);
+
+            }
+        }
+    }
+    public class BinaryTree
+    {
+        private TreeNode root;
+        public void Add(int value)
+        {
+            if(root != null)
+            {
+                root.Insert(value);
+            }
+            else
+            {
+                root = new TreeNode { Data = value };
+            }
+        }
+        public void Print()
+        {
+            List<int> result = new List<int>();
+            root.CLR(root, result);
+
+            result.ForEach(x => Console.Write(x + " "));
+        }
+        public void PrintSorted()
+        {
+            List<int> result = new List<int>();
+            root.LCR(root, result);
+
+            result.ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 4 3 1 9 8 6 7 15 10
+            BinaryTree bt = new BinaryTree();
+            bt.Add(5);
+            bt.Add(9);
+            bt.Add(4);
+            bt.Add(3);
+            bt.Add(8);
+            bt.Add(15);
+            bt.Add(1);
+            bt.Add(6);
+            bt.Add(10);
+            bt.Add(7);
+            bt.Print();
+            Console.WriteLine();
+            bt.PrintSorted();
+
+        }
+    }
+}
